659b104b286e64794c289f3a5a1aa3b20f8b8611
NoodleNTMY <149809329+NoodleNTMY@users.noreply.github.com>
2024-04-03 17:23:03 +0000

    Java

---
+++ Todo/UD7 - Javascript/Ejemplo clase/script.js
@@ -0,0 +1,10 @@
+let MENSAJE = "Hola Mundo"
+
+console.log("¿Hola Mundo?");
+alert("Hola ASIRB-B");
+
+
+mensaje = "Adios Mundo"
+
+// Las constantes siempre se ponen en mayusculas y con guiones bajos siempre y
+// cuando la constante que introduzcamos sepamos el valor.
