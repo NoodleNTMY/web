1d3aa3ef1bad67e9c9a6be21d89db089afaac6d1
NoodleNTMY <149809329+NoodleNTMY@users.noreply.github.com>
2024-04-10 15:54:57 +0000

    yes

---
+++ Todo/UD7 - Javascript/E-12.3.1 Guillermo César/if-else.js
@@ -0,0 +1,7 @@
+let pregunta = prompt ('¿Cual es el nombre oficial de JavaScript?');
+
+if (pregunta == "ECMAScript"){
+    alert( '¡Correcto!');
+} else {
+    alert( 'Chuungo la peluca, es ECMAScript' );
+}
