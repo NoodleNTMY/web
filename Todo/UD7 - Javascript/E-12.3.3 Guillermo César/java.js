12bb11265ad846cb6c1a140e74beb9f1ee7e081e
NoodleNTMY <149809329+NoodleNTMY@users.noreply.github.com>
2024-04-10 16:38:27 +0000

    12.3.3

---
+++ Todo/UD7 - Javascript/E-12.3.3 Guillermo César/java.js
@@ -0,0 +1,7 @@
+let age = prompt ( 'Escriba por pantalla su edad:' );
+
+if ( age <= 90, age >= 14) {
+    alert ( 'Tu edad está entre 14 y 90 años.' )
+} else {
+    alert ( 'Chungo brother, o muy nano o muy boomer.' )
+}
