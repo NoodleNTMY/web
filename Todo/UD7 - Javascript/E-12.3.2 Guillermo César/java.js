ca050458e4936c8f0d421428925cb009cb7875e8
NoodleNTMY <149809329+NoodleNTMY@users.noreply.github.com>
2024-04-10 16:27:36 +0000

    segundo

---
+++ Todo/UD7 - Javascript/E-12.3.2 Guillermo César/java.js
@@ -0,0 +1,7 @@
+let numero = prompt ( 'Tecleeme un número:' );
+
+if ( numero > 100 ) {
+    alert( 'Efectivamente el numero introducido es mayor que 100' );
+} else {
+    alert( 'Efectivamente el numero introducido no es mayor que 100' );
+}
