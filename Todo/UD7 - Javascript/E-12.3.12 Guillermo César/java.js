93389873b34ba219aa208ac0b97ffafdd2a154c9
NoodleNTMY <149809329+NoodleNTMY@users.noreply.github.com>
2024-04-12 14:31:50 +0000

    correción

--- Todo/UD7 - Javascript/E-12.3.12 Guillermo César/java.js
+++ Todo/UD7 - Javascript/E-12.3.12 Guillermo César/java.js
@@ -3,9 +3,7 @@ let dia = prompt ('Introduce un valor del 1 al 7:');
 
 if (isNaN(dia)) {
     alert ( 'El valor introducido no es numérico' );
-} else if ( dia < 1 ) {
-    alert ( 'Valor invalido' );
-} else if ( dia > 7 ) {
+} else if (( dia < 1 ) || ( dia > 7 )) {
     alert ( 'Valor invalido' );
 } else {
     alert ( 'El dia de la semana correspondiente es: '+ dias_semana[--dia]);
