2113b6d57cf9b2dfc04c87c91eb9cdb3a601511d
NoodleNTMY <149809329+NoodleNTMY@users.noreply.github.com>
2024-04-12 13:34:11 +0000

    13.3.8

---
+++ Todo/UD7 - Javascript/E-12.3.8 Guillermo César/java.js
@@ -0,0 +1,9 @@
+let num = prompt ( 'Escriba por pantalla un número:' );
+
+if ( num % 2 == 0 && num != 0) {
+    alert ( 'El número introducido es par.' )
+} else if ( num * 1 == 0 ) {
+    alert ( 'El número introducido es 0.' )
+} else {
+    alert ( 'El número introducido es impar.' )
+}
