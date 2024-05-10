da5d4a74fbda337cbcf2d5e57b8b86e36f2919e3
NoodleNTMY <149809329+NoodleNTMY@users.noreply.github.com>
2024-05-10 14:25:57 +0000

    array

--- Todo/UD7 - Javascript/E-FUNC pares array Guillermo César/script.js
+++ Todo/UD7 - Javascript/E-FUNC pares array Guillermo César/script.js
@@ -6,9 +6,20 @@ function PrimerosPares (num){
     return PARES;
 }
 
-function MostrarPares(num){
-    
+function MostrarPares(a){
+    let mostrartxt = 'Salida: ';
+
+    for (let i = 0; i < a.length; i++){
+        if (i == a.length-1){
+            mostrartxt = mostrartxt + a[i] + '.';
+        } else {
+            mostrartxt = mostrartxt + a[i] + ', ';
+        }
+    }
+
+    console.log(mostrartxt);
 }
 
 let num = prompt("Introduzca un numero:");
 let valor = PrimerosPares (num);
+MostrarPares (valor);
