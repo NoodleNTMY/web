0007174f2341a41f49a2e22b3ba6e8622ab1f22b
NoodleNTMY <149809329+NoodleNTMY@users.noreply.github.com>
2024-05-29 15:49:34 +0000

    inicio proyecto

---
+++ Proyecto AVASCRIPT/Ejercicio 1/java/script.js
@@ -0,0 +1,13 @@
+/* Guillermo César García Brito */
+
+/* Primero creamos el prompt con el menú */
+
+/*
+if (age == null) {
+    alert ( 'Debes introducir algún valor.' );
+} else if (isNaN(age)){
+    alert ( 'Los valores introducidos no son numéricos.' );
+*/
+    do { 
+        let valor = prompt ('Escriba por pantalla el número del comando que desee ejeecutar, siendo : 1.- nPrimos 2.- Serie Fibonacci 3.-DNI Váido 4.-Lanza Moneda 5.-Última letra 0.-SALIR')
+    } while (valor == [0 - 5] && valor != Null);
